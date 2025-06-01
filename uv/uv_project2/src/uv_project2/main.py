0c92e3a93bde866ba7273edcddb7ca6a5e82da79
Zehra Naveed <162749372+zehranaveedt@users.noreply.github.com>
2025-06-02 03:22:00 +0500

    Add files via upload

---
+++ uv/uv_project2/src/uv_project2/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello! Welcome to uv_project2.")
+
+if __name__ == "__main__":
+    main()
+    print("This is the main module of uv_project2.")
+    print("You can run this script directly to see how it works.")
+    print("Or you can import its functions into another Python file.")
+    print("This is a simple Python project set up using the UV package manager.")
